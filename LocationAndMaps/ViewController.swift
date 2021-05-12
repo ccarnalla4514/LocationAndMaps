9a9e4754f6591eccf406c47c9b05427de296d044
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-05-12 15:21:45 -0500

    LocationAndMapsLecture

--- LocationAndMaps/ViewController.swift
+++ LocationAndMaps/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreLocation
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userLocation.title = "My Location"
+    
+    addPinAnnotationToMapView()
+    
     }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            textView.text = "\(location)"
+        }
+    }
+    func addPinAnnotationToMapView(){
+        let coordinate = CLLocationCoordinate2D(latitude: 68, longitude: -12.4230)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "Title"
+        annotation.subtitle = "Subtitle"
+        mapView.addAnnotation(annotation)
+        setCenterOfMapToLocation(location: coordinate)
+    }
+    func setCenterOfMapToLocation(location: CLLocationCoordinate2D){
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    
 
 
 }
-
